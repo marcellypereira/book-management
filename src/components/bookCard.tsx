a0531ac15a9e8e96e38301c3840ff095ffecf1b4
marcellypereira <marcellyp120@gmail.com>
2024-12-01 03:00:08 -0300

    :sparkles: feat: Acesso usuário comum

---
+++ src/components/bookCard.tsx
@@ -0,0 +1,112 @@
+import React, { useState } from "react";
+import { FiEdit2, FiTrash } from "react-icons/fi";
+import { useBooks } from "../hooks/useBooks";
+import { useNavigate } from "react-router-dom";
+import DeleteModal from "../pages/book/deleteBook";
+import PaginatedList from "../components/paginatedList";
+
+const BookList: React.FC = () => {
+  const { books, deleteBook } = useBooks();
+  const navigate = useNavigate();
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [selectedBookId, setSelectedBookId] = useState<string | null>(null);
+
+  const openModal = (bookId: string) => {
+    setSelectedBookId(bookId);
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setSelectedBookId(null);
+    setModalOpen(false);
+  };
+
+  const handleDelete = () => {
+    if (selectedBookId !== null) {
+      deleteBook.mutate(selectedBookId, {
+        onSuccess: () => {
+          console.log(`Livro com ID ${selectedBookId} foi excluído.`);
+          closeModal();
+        },
+        onError: () => {
+          console.error("Erro ao excluir o livro.");
+        },
+      });
+    }
+  };
+
+  if (books.isLoading) {
+    return <p>Carregando...</p>;
+  }
+
+  if (books.isError) {
+    return <p>Erro ao carregar os livros.</p>;
+  }
+
+  return (
+    <div className="grid gap-4">
+      <div className="border-t border-gray-300 mb-2 mt-5"></div>
+  
+      <div className="hidden sm:grid grid-cols-[6rem,2.5fr,1fr,1fr,6rem] gap-x-8 gap-y-4 text-gray-400 text-sm">
+        <span className="font-medium text-center">Capa</span>
+        <span className="font-medium text-center">Nome</span>
+        <span className="font-medium text-center">Autor</span>
+        <span className="font-medium text-center">Data</span>
+        <span className="font-medium text-center">Ações</span>
+      </div>
+  
+      <PaginatedList
+        items={books.data || []}
+        itemsPerPage={5}
+        renderItem={(book) => (
+          <div
+            key={book.id}
+            className="grid sm:grid-cols-[6rem,2.5fr,1fr,1fr,6rem] gap-x-8 gap-y-4 items-center px-4 py-2 bg-white rounded-md shadow-md sm:text-start lg:text-center md:text-center"
+          >
+            <div className="w-full h-24 sm:h-28 mx-auto sm:mx-0">
+              <img
+                src={book.coverImage}
+                alt={book.title}
+                className="w-full h-full object-cover rounded-md"
+              />
+            </div>
+  
+            <h2 className="text-md font-semibold text-gray-900 truncate">
+              {book.title}
+            </h2>
+  
+            <p className="text-sm text-gray-500 truncate">{book.author}</p>
+  
+            <p className="text-sm text-gray-500 truncate">{book.publicationDate}</p>
+  
+            <div className="flex lg:justify-center md:justify-center justify-end space-x-4">
+              <button
+                className="text-[#6347F9] hover:text-[#5037d1] transition-all"
+                onClick={() => navigate(`/edit-book/${book.id}`)}
+              >
+                <FiEdit2 size={20} />
+              </button>
+              <button
+                className="text-[#6347F9] hover:text-[#5037d1] transition-all"
+                onClick={() => book.id && openModal(String(book.id))}
+              >
+                <FiTrash size={20} />
+              </button>
+            </div>
+          </div>
+        )}
+      />
+  
+      <DeleteModal
+        isOpen={isModalOpen}
+        onClose={closeModal}
+        onConfirm={handleDelete}
+      />
+    </div>
+  );
+  
+  
+  
+};
+
+export default BookList;
---
+++ src/pages/listForUsers.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { FiLogOut } from "react-icons/fi";
+import { useNavigate } from "react-router-dom";
+import Title from "../components/title";
+import BookCard from "../components/bookCard";
+import { useBooks } from "../hooks/useBooks";
+
+const ListForUsers: React.FC = () => {
+  const { books } = useBooks();
+  const navigate = useNavigate();
+
+  if (books.isLoading) {
+    return <div>Carregando...</div>;
+  }
+
+  if (books.isError) {
+    return <div>Erro ao carregar livros: {books.error?.message}</div>;
+  }
+
+  return (
+    <div className="p-8">
+      <Title text="Books Management" />
+
+      <div className="mt-auto px-6 pb-4">
+        <button
+          className="absolute top-8 right-8 flex items-center p-2 text-[#6347F9] hover:text-[#5238e5]"
+          onClick={() => navigate("/signin")}
+        >
+          <FiLogOut className="mr-2 text-xl" />
+          Sair
+        </button>
+      </div>
+
+      <div className="mt-10">
+        <h1 className="text-xl font-bold text-black">Lista de Livros</h1>
+        <div className="border-t border-gray-300 mb-2 mt-5"></div>
+      </div>
+
+      <div className="mt-16 px-4 sm:px-6 md:px-8 lg:px-12 xl:px-16 grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-6 justify-items-center">
+        {books.data?.map((book, index) => (
+          <div key={index} className="max-w-xs">
+            <BookCard
+              title={book.title}
+              author={book.author}
+              imageUrl={book.coverImage}
+              summary={book.summary}
+              publicationDate={book.publicationDate}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ListForUsers;
